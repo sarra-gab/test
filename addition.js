cab10b6172272d332c2093c5d1eafb3e42bec4c6
sarra-gab <gabbassisarra1997@gmail.com>
2022-10-25 16:24:00 +0100

    Initial release

---
+++ addition.js
@@ -0,0 +1,2 @@
+var Addition=require('./index2.js');
+console.log(Addition.AddNumber(1,2));
---
+++ index2.js
@@ -0,0 +1,8 @@
+var exports=module.exports={};
+exports.AddNumber=function(a,b)
+{
+return a+b;
+};
+
+/*var Addition=require('./index2.js');
+console.log(Addition.AddNumber(1,2));*/
